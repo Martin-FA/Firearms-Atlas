a943fdbd55474e936745c4d36f2f9a829afa4530
Martin-FA <jurkovic.martin@icloud.com>
2025-05-27 11:16:34 +0200

    Update ammo.js

--- ammo.js
+++ ammo.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('data/ammo.json')
+  fetch('ammo.json')
     .then(response => response.json())
     .then(data => {
       const container = document.getElementById('ammo-container');
